23bc1aed914da05dc0e621141cbcec872fb59483
tomasz <csharp@406-00>
2026-07-02 13:45:06 +0200

    q

---
+++ P07DataBinding/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P07DataBinding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string sciezka = @"C:\Users\csharp\source\repos\Dzien4\P04AplikacjaZawodnicy\zawodnicy.txt";
+
+        private void btnWczytaj_Click(object sender, EventArgs e)
+        {
+            ManagerZawodnikow mz = new ManagerZawodnikow();
+            Zawodnik[] zawodnicy = mz.WczytajZawodnikow(sciezka);
+
+            lbDane.DataSource = zawodnicy; // wiązanie danych z listboxem
+            lbDane.DisplayMember = "ImieNazwisko"; // wyświetlanie imienia zawodnika w listboxie
+        }
+
+        private void btnPodajTemperature_Click(object sender, EventArgs e)
+        {
+            // checmy odczytać, który zawodnik jest aktualnie zaznaczony 
+            Zawodnik zaznaczony = (Zawodnik)lbDane.SelectedItem;
+
+            MessageBox.Show(zaznaczony.ImieNazwisko);
+        }
+    }
+}
---
+++ P07DataBinding/ManagerZawodnikow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07DataBinding
+{
+    internal class ManagerZawodnikow
+    {
+        public Zawodnik[] WczytajZawodnikow(string sciezka)
+        {
+
+            //string[] wiersze = File.ReadAllText(sciezka).Split(new string[1] {"\r\n"},StringSplitOptions.RemoveEmptyEntries);
+            string[] wiersze =  File.ReadAllLines(sciezka);
+
+            Zawodnik[] zawodnicy = new Zawodnik[wiersze.Length-1];
+
+            for (int i = 1; i < wiersze.Length; i++)
+            {
+                string[] komorki =  wiersze[i].Split(',');
+
+                Zawodnik z = new Zawodnik();
+                z.Id_zawodnika = int.Parse(komorki[0]);
+                if (!string.IsNullOrEmpty(komorki[1]))
+                    z.Id_trenera = int.Parse(komorki[1]);
+
+                z.Imie = komorki[2];
+                z.Nazwisko = komorki[3];
+                z.Kraj = komorki[4];
+                z.DataUrodzenia = DateTime.Parse(komorki[5]);
+                z.Wzrost = int.Parse(komorki[6]);
+                z.Waga = int.Parse(komorki[7]);
+
+                zawodnicy[i - 1] = z;
+
+            }
+            return zawodnicy;
+        }
+
+    }
+}
