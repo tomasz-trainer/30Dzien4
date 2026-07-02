dea33e96a84acaa14b709161b5a76126e89e3941
tomasz <csharp@406-00>
2026-07-02 08:57:15 +0200

    Add project files.

---
+++ P01AplikacjaOkienkowa/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace P01AplikacjaOkienkowa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLiczba1 = new System.Windows.Forms.TextBox();
+            this.txtLiczba2 = new System.Windows.Forms.TextBox();
+            this.lblWynik = new System.Windows.Forms.Label();
+            this.btnPolicz = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLiczba1
+            // 
+            this.txtLiczba1.Location = new System.Drawing.Point(31, 40);
+            this.txtLiczba1.Name = "txtLiczba1";
+            this.txtLiczba1.Size = new System.Drawing.Size(100, 20);
+            this.txtLiczba1.TabIndex = 0;
+            // 
+            // txtLiczba2
+            // 
+            this.txtLiczba2.Location = new System.Drawing.Point(147, 40);
+            this.txtLiczba2.Name = "txtLiczba2";
+            this.txtLiczba2.Size = new System.Drawing.Size(100, 20);
+            this.txtLiczba2.TabIndex = 1;
+            // 
+            // lblWynik
+            // 
+            this.lblWynik.AutoSize = true;
+            this.lblWynik.Location = new System.Drawing.Point(178, 80);
+            this.lblWynik.Name = "lblWynik";
+            this.lblWynik.Size = new System.Drawing.Size(35, 13);
+            this.lblWynik.TabIndex = 2;
+            this.lblWynik.Text = "label1";
+            // 
+            // btnPolicz
+            // 
+            this.btnPolicz.Location = new System.Drawing.Point(31, 75);
+            this.btnPolicz.Name = "btnPolicz";
+            this.btnPolicz.Size = new System.Drawing.Size(100, 23);
+            this.btnPolicz.TabIndex = 3;
+            this.btnPolicz.Text = "Policz";
+            this.btnPolicz.UseVisualStyleBackColor = true;
+            this.btnPolicz.Click += new System.EventHandler(this.btnPolicz_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Liczba 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(144, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Liczba 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 151);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnPolicz);
+            this.Controls.Add(this.lblWynik);
+            this.Controls.Add(this.txtLiczba2);
+            this.Controls.Add(this.txtLiczba1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLiczba1;
+        private System.Windows.Forms.TextBox txtLiczba2;
+        private System.Windows.Forms.Label lblWynik;
+        private System.Windows.Forms.Button btnPolicz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ P02KlasyCzesciowe/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("P02KlasyCzesciowe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P02KlasyCzesciowe")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e10b187d-29c9-499a-b60e-e881e3f64ad9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
